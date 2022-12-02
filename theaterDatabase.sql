2927a6e5bc721f221be868903683d805f18d445e
Ahmad <100788678+AhmadJanjua@users.noreply.github.com>
2022-12-02 15:05:40 -0700

    Added SQL file

---
+++ theaterDatabase.sql
@@ -0,0 +1,135 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "time_seat" (
+	"theatre"	TEXT,
+	"room number"	INTEGER,
+	"movie_name"	TEXT,
+	"year"	 INTEGER,
+	"month"	 INTEGER,
+	"day"	 INTEGER,
+	"hour"	 INTEGER,
+	"minute" INTEGER,
+	"seat"	TEXT,
+	"MemberOnly" BIT,
+	"Booked" BIT
+);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 4, 10, 15, 'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 4, 10, 15, 'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 4, 10, 15, 'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 4, 10, 15, 'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 4, 10, 15, 'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 4, 10, 15, 'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 4, 10, 15, 'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 4, 10, 15, 'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 4, 10, 15, 'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 4, 10, 15, 'b5',1, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 4, 10, 15, 'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 4, 10, 15, 'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 4, 10, 15, 'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 4, 10, 15, 'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 4, 10, 15, 'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 4, 10, 15, 'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 4, 10, 15, 'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 4, 10, 15, 'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 4, 10, 15, 'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 4, 10, 15, 'b5',1, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 4, 12, 15, 'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 4, 12, 15, 'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 4, 12, 15, 'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 4, 12, 15, 'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 4, 12, 15, 'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 4, 12, 15, 'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 4, 12, 15, 'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 4, 12, 15, 'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 4, 12, 15, 'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 4, 12, 15, 'b5',1, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 4, 12, 15, 'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 4, 12, 15, 'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 4, 12, 15, 'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 4, 12, 15, 'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 4, 12, 15, 'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 4, 12, 15, 'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 4, 12, 15, 'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 4, 12, 15, 'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 4, 12, 15, 'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 4, 12, 15, 'b5',1, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Spiderman',2022,12, 7, 10, 15, 'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Spiderman',2022,12, 7, 10, 15, 'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Spiderman',2022,12, 7, 10, 15, 'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Spiderman',2022,12, 7, 10, 15, 'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Spiderman',2022,12, 7, 10, 15, 'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Spiderman',2022,12, 7, 10, 15, 'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Spiderman',2022,12, 7, 10, 15, 'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Spiderman',2022,12, 7, 10, 15, 'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Spiderman',2022,12, 7, 10, 15, 'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Spiderman',2022,12, 7, 10, 15, 'b5',1, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Avatar',2022,12, 7, 10, 15, 'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Avatar',2022,12, 7, 10, 15, 'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Avatar',2022,12, 7, 10, 15, 'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Avatar',2022,12, 7, 10, 15, 'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Avatar',2022,12, 7, 10, 15, 'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Avatar',2022,12, 7, 10, 15, 'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Avatar',2022,12, 7, 10, 15, 'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Avatar',2022,12, 7, 10, 15, 'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Avatar',2022,12, 7, 10, 15, 'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Avatar',2022,12, 7, 10, 15, 'b5',1, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Puss In Boots',2022,12, 7, 14, 15, 'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Puss In Boots',2022,12, 7, 14, 15, 'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Puss In Boots',2022,12, 7, 14, 15, 'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Puss In Boots',2022,12, 7, 14, 15, 'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Puss In Boots',2022,12, 7, 14, 15, 'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Puss In Boots',2022,12, 7, 14, 15, 'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Puss In Boots',2022,12, 7, 14, 15, 'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Puss In Boots',2022,12, 7, 14, 15, 'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Puss In Boots',2022,12, 7, 14, 15, 'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Puss In Boots',2022,12, 7, 14, 15, 'b5',1, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Black Panther',2022,12, 7, 14, 15, 'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Black Panther',2022,12, 7, 14, 15, 'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Black Panther',2022,12, 7, 14, 15, 'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Black Panther',2022,12, 7, 14, 15, 'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Black Panther',2022,12, 7, 14, 15, 'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Black Panther',2022,12, 7, 14, 15, 'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Black Panther',2022,12, 7, 14, 15, 'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Black Panther',2022,12, 7, 14, 15, 'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Black Panther',2022,12, 7, 14, 15, 'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Black Panther',2022,12, 7, 14, 15, 'b5',1, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 9, 16, 15, 'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 9, 16, 15, 'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 9, 16, 15, 'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 9, 16, 15, 'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 9, 16, 15, 'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 9, 16, 15, 'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 9, 16, 15, 'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 9, 16, 15, 'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 9, 16, 15, 'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Black Panther',2022,12, 9, 16, 15, 'b5',1, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 9, 16, 15, 'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 9, 16, 15, 'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 9, 16, 15, 'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 9, 16, 15, 'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 9, 16, 15, 'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 9, 16, 15, 'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 9, 16, 15, 'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 9, 16, 15, 'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 9, 16, 15, 'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Puss In Boots',2022,12, 9, 16, 15, 'b5',1, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 9, 17, 15,'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 9, 17, 15,'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 9, 17, 15,'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 9, 17, 15,'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 9, 17, 15,'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 9, 17, 15,'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 9, 17, 15,'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 9, 17, 15,'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 9, 17, 15,'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',1,'Avatar',2022,12, 9, 17, 15,'b5',1, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 9, 18, 15, 'a1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 9, 18, 15, 'a2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 9, 18, 15, 'a3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 9, 18, 15, 'a4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 9, 18, 15, 'a5',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 9, 18, 15, 'b1',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 9, 18, 15, 'b2',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 9, 18, 15, 'b3',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 9, 18, 15, 'b4',0, 0);
+INSERT INTO "time_seat" VALUES ('Theater480',2,'Spiderman',2022,12, 9, 18, 15, 'b5',1, 0);
+COMMIT;
