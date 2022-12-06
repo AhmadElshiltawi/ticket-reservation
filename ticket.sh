cc736ff08e8e38eccb7990815e7c1b67d1e17055
Ahmad Elshiltawi <85766582+AhmadElshiltawi@users.noreply.github.com>
2022-12-05 19:21:28 -0700

    Add files via upload

---
+++ ticket.sh
@@ -0,0 +1,1 @@
+java -jar --enable-preview  --module-path "./referencedlibraries" --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.web --add-exports javafx.graphics/com.sun.javafx.sg.prism=ALL-UNNAMED ticket-reservation.jar
