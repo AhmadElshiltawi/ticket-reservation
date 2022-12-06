b3aca2f42d5b87aa8b7cc45574e9a504c8a88247
Ahmad <100788678+AhmadJanjua@users.noreply.github.com>
2022-12-05 19:58:50 -0700

    Deleted Extra files

--- ticket.sh
+++
@@ -1,1 +0,0 @@
-java -jar --enable-preview  --module-path "./referencedlibraries" --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.web --add-exports javafx.graphics/com.sun.javafx.sg.prism=ALL-UNNAMED ticket-reservation.jar
